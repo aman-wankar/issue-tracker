c8395fa149a9c8ad8561fd184109fffd9d78a72e
809902 <amanwankar07@gmail.com>
2023-12-22 16:06:08 +0530

    Style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,18 @@
+'use client';
+
+import classNames from 'classnames';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { AiFillBug } from 'react-icons/ai';
 
 const NavBar = () => {
+  const currentPathname = usePathname();
+
   const links = [
     { label: 'Dashboard', href: '/' },
     { label: 'Issues', href: '/issues' },
   ];
-  
+
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
       <Link href='/'>
@@ -17,7 +23,11 @@ const NavBar = () => {
           <Link
             key={index}
             href={link.href}
-            className='text-zinc-500 hover:text-zinc-800 transition-colors'
+            className={classNames({
+              'text-zinc-900': currentPathname === link.href,
+              'text-zinc-500': currentPathname !== link.href,
+              'hover:text-zinc-800 transition-colors': true,
+            })}
           >
             {link.label}
           </Link>
