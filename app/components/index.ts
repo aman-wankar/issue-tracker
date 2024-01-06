89014ca4cb95f3de67536702cf9abf3b4e53d461
809902 <amanwankar07@gmail.com>
2024-01-06 21:28:17 +0530

    Refactor: Organize import statements

---
+++ app/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Skeleton } from 'react-loading-skeleton';
+export { default as ErrorMessage } from './ErrorMessage';
+export { default as IssueStatusBadge } from './IssueStatusBadge';
+export { default as Link } from './Link';
+export { default as Spinner } from './Spinner';
+
