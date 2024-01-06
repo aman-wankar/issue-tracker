7c6366f254d892c6ba77baf184e011b3110dd710
809902 <amanwankar07@gmail.com>
2024-01-06 22:03:22 +0530

    Added edit button

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,4 +1,4 @@
-export { default as Skeleton } from 'react-loading-skeleton';
+export { default as Skeleton } from './Skeleton';
 export { default as ErrorMessage } from './ErrorMessage';
 export { default as IssueStatusBadge } from './IssueStatusBadge';
 export { default as Link } from './Link';
